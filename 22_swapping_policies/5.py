88734d10aef752d74f1de35b86864f9d8d39b67c
swarajd <swaraj.dhumne@gmail.com>
2019-12-21 09:38:19 -0500

    shift by 12 because pages are 4kb

--- 22_swapping_policies/5.py
+++ 22_swapping_policies/5.py
@@ -5,7 +5,7 @@ with open("ls.txt", "r+") as f:
         if not line.startswith("="):
             parts = line.strip().split()
             address_str = parts[1].split(",")[0]
-            address = int(address_str, 16) >> 11
+            address = int(address_str, 16) >> 12
             print(address)
 
 # print(",".join(addresses))
