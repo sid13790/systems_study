59fe25b686fb4a9f5e7bd8d8dd21018e2225e9ac
swarajd <swaraj.dhumne@gmail.com>
2019-12-25 22:39:29 -0500

    finished Q6 of ch 31 HW

--- 31_semaphores/mutex-nostarve.c
+++ 31_semaphores/mutex-nostarve.c
@@ -10,16 +10,52 @@
 // when attempting to acquire this mutex you build?
 //
 
-typedef __ns_mutex_t {
+typedef struct __ns_mutex_t {
+    int room1;
+    int room2;
+    sem_t mutex;
+    sem_t t1;
+    sem_t t2;
 } ns_mutex_t;
 
 void ns_mutex_init(ns_mutex_t *m) {
+    m->room1 = 0;
+    m->room2 = 0;
+    Sem_init(&m->mutex, 1);
+    Sem_init(&m->t1, 1);
+    Sem_init(&m->t2, 0);
 }
 
 void ns_mutex_acquire(ns_mutex_t *m) {
+    Sem_wait(&m->mutex);
+    m->room1++;
+    Sem_post(&m->mutex);
+
+    Sem_wait(&m->t1);
+
+    m->room2++;
+    Sem_wait(&m->mutex);
+    m->room1--;
+
+    if (m->room1 == 0) {
+        Sem_post(&m->mutex);
+        Sem_post(&m->t2);
+    } else {
+        Sem_post(&m->mutex);
+        Sem_post(&m->t1);
+    }
+
+    Sem_wait(&m->t2);
+
+    m->room2--;
 }
 
 void ns_mutex_release(ns_mutex_t *m) {
+    if (m->room2 == 0) {
+        Sem_post(&m->t1);
+    } else {
+        Sem_post(&m->t2);
+    }
 }
 
 
